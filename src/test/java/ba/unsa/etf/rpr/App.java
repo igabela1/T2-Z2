01d149ab9730d3c9bb1df1af81a5311d2e9685da
Ilhana Gabela <igabela1@etf.unsa.ba>
2022-10-26 22:58:29 +0200

    Initial commit

---
+++ src/test/java/ba/unsa/etf/rpr/App.java
@@ -0,0 +1,22 @@
+package ba.unsa.etf.rpr;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class App
+{
+
+
+
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
